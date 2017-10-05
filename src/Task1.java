733a86b0441ff577273d7bf8c9924192d14ed40b
Kiota <turo-yulya@yandex.ru>
2017-10-05 18:13:32 +0300

    first commit

---
+++ src/Task1.java
@@ -0,0 +1,40 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Юлия on 05.10.2017.
+ */
+public class Task1 {
+
+    public static void main (String args []) {
+
+    // используя %
+    private static int gcd1(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    // используя floorMod
+    private static int gcd2(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    // используя rem()
+    private static int gcd3(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
+}
+
+}
+}
